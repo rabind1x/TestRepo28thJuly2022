783ac77e7cc5063b4a0f804cfe4d7bdfc91d93b6
Tarablus, Yuval <yuval.tarablus@intel.com>
2020-05-13 18:06:55 +0000

    FA Invoke Tester
    git-tfs-id: [http://tfs.ltdauto.intel.com:8080/tfs/defaultcollection]$/APC/source/Tools/FaInvokeTester;C527841

---
+++ FAInvoke/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace FAInvoke
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInvoke = new System.Windows.Forms.Button();
+            this.txtMethod = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReply = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRichInput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnInvoke
+            // 
+            this.btnInvoke.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInvoke.Location = new System.Drawing.Point(602, 26);
+            this.btnInvoke.Name = "btnInvoke";
+            this.btnInvoke.Size = new System.Drawing.Size(116, 23);
+            this.btnInvoke.TabIndex = 0;
+            this.btnInvoke.Text = "Invoke";
+            this.btnInvoke.UseVisualStyleBackColor = true;
+            this.btnInvoke.Click += new System.EventHandler(this.btnInvoke_Click);
+            // 
+            // txtMethod
+            // 
+            this.txtMethod.Location = new System.Drawing.Point(61, 28);
+            this.txtMethod.Name = "txtMethod";
+            this.txtMethod.Size = new System.Drawing.Size(508, 20);
+            this.txtMethod.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Method";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input";
+            // 
+            // txtReply
+            // 
+            this.txtReply.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReply.Location = new System.Drawing.Point(61, 272);
+            this.txtReply.Multiline = true;
+            this.txtReply.Name = "txtReply";
+            this.txtReply.ReadOnly = true;
+            this.txtReply.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtReply.Size = new System.Drawing.Size(677, 301);
+            this.txtReply.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Reply";
+            // 
+            // txtRichInput
+            // 
+            this.txtRichInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRichInput.Location = new System.Drawing.Point(61, 73);
+            this.txtRichInput.Name = "txtRichInput";
+            this.txtRichInput.Size = new System.Drawing.Size(677, 180);
+            this.txtRichInput.TabIndex = 4;
+            this.txtRichInput.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 597);
+            this.Controls.Add(this.txtRichInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtReply);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMethod);
+            this.Controls.Add(this.btnInvoke);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "FA Invoke Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInvoke;
+        private System.Windows.Forms.TextBox txtMethod;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReply;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtRichInput;
+    }
+}
+
---
+++ FAInvoke/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Intel.FabAuto.FA.NET.CoreFBOL;
+using Intel.FabAuto.FA.NET.ClientFBOL.Gen;
+using System.Xml.Linq;
+using Intel.FabAuto.FWServices.Instrumentation;
+using Intel.FabAuto.FA.NET.Common.BaseClasses;
+using Intel.FabAuto.FA.NET.Common;
+using Intel.FabAuto.FA.NET.Client;
+using System.Diagnostics;
+
+namespace FAInvoke
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInvoke_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtReply.Text = "Invoked....";
+                btnInvoke.Enabled = false;
+                this.Refresh();
+                GenHelper genHelper = new GenHelper();
+               // string head2 = @"<Header FA:dt=""struct""><UserCredentials FA:dt=""struct""><Username dt:dt=""string"">RF3PROD\RF3_SCUser</Username></UserCredentials><TransactionInformation FA:dt=""struct""><OriginatingSite dt:dt=""string"">D1D</OriginatingSite><OriginatingEnvironment dt:dt=""string"">PROD.0</OriginatingEnvironment><TargetSite dt:dt=""string"">D1D</TargetSite><TargetEnvironment dt:dt=""string"">PROD.0</TargetEnvironment><Hostname dt:dt=""string"">R3PEDT410U</Hostname><ClientID dt:dt=""string"">NTSC</ClientID><TransactionID dt:dt=""string"">3695ad5c-0a1c-46dd-a9d8-c201bfd3a70c</TransactionID><Trace dt:dt=""boolean"">0</Trace><Timestamp dt:dt=""string"">2016-10-23T23:02:07.538-07:00</Timestamp><Timeout dt:dt=""i4"">200</Timeout></TransactionInformation><RoutingInfo FA:dt=""struct""><ClientCapabilityList FA:dt=""list""><item dt:dt=""string"">RCTGate</item><item dt:dt=""string"">AutoSIFDownload</item><item dt:dt=""string"">EPA</item><item dt:dt=""string"">AutoSIF</item></ClientCapabilityList><YASProcessInfo FA:dt=""struct""/></RoutingInfo></Header>";
+              //  string opDetails = @"<Envelope FA:dt=""struct"" xmlns:dt=""urn:schemas-microsoft-com:datatypes"" xmlns:FA=""urn:Intel-FabAuto-FA300"">" + head2 + @"<Body FA:dt=""struct""> <Name dt:dt=""string"">0300</Name> </Body></Envelope>";
+                XElement xReq = XElement.Parse(txtRichInput.Text);
+                string methodName = txtMethod.Text;
+                XElement response = genHelper.Invoke(methodName, xReq, Guid.NewGuid(), Guid.NewGuid(), "ApcFw30", Process.GetCurrentProcess().ProcessName);
+                txtReply.Text = response.ToString();
+            }
+            catch(Exception ex)
+            {
+                txtReply.Text = $"Invoke failed. Message:{ex.Message}";
+                if (ex.InnerException == null)
+                    txtReply.Text += "; No inner Exception";
+                while (ex.InnerException!=null)
+                {
+                    ex = ex.InnerException;
+                    txtReply.Text += "; Inner Source: " + ex.Source + "; Message:" + ex.Message;                    
+                }                
+            }
+            finally
+            {
+                btnInvoke.Enabled = true;
+            }
+
+        }
+    }
+}
