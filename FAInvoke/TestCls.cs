47b158efc6592e4e4bf47ecb25130484fa2ea45a
Das, RabindranathX <rabindranathx.das@intel.com>
2022-03-30 07:49:04 +0000

    Add a new class for test only
    git-tfs-id: [http://tfs.ltdauto.intel.com:8080/tfs/defaultcollection]$/APC/source/Tools/FaInvokeTester;C654706

---
+++ FAInvoke/TestCls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FAInvoke
+{
+    class TestCls
+    {
+        //only for test Rabi 30th-Mar-2022
+    }
+}
